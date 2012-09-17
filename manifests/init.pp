8affd8a8261f66fa2eaabe7f96f4eba9ba1c93b7
Simon Josi <simon.josi@atizo.com>
2012-09-17 18:20:06 +0200

    uwsgi initscript has no proper status support

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,7 +19,7 @@ class uwsgi(
   service{'uwsgi':
     ensure => running,
     enable => true,
-    hasstatus => true,
+    hasstatus => false,
     require => Package['uwsgi'],
   }
   if $sysconfig {
